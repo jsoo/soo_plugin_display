4bcad95c5a099cb78c793eca4aa3b962901e7a1c
Jeff <jeff@ipsedixit.net>
2009-09-28 15:53:54 -0400

    Version 0.1.3
    soo_plugin_code tab to space conversion now aligns to tab stops

--- soo_plugin_display.php
+++ soo_plugin_display.php
@@ -1,7 +1,7 @@
 <?php
 
 $plugin['name'] = 'soo_plugin_display';
-$plugin['version'] = '0.1.2';
+$plugin['version'] = '0.1.3';
 $plugin['author'] = 'Jeff Soo';
 $plugin['author_uri'] = 'http://ipsedixit.net/txp/';
 $plugin['description'] = 'Display info about installed plugins';
@@ -29,17 +29,23 @@ add_privs('plugin_lifecycle.soo_plugin_display','1,2');
 register_callback('soo_plugin_display_prefs', 'plugin_prefs.soo_plugin_display');
 register_callback('soo_plugin_display_prefs', 'plugin_lifecycle.soo_plugin_display');
 
-if ( function_exists('soo_plugin_pref_vals') )
-	$soo_plugin_display_prefs = soo_plugin_pref_vals('soo_plugin_display');
-else 
-	foreach ( soo_plugin_display_defaults() as $name => $val )
-		$soo_plugin_display_prefs[$name] = $val;
+	// load prefs if soo_plugin_pref enabled, else load defaults
+$soo_plugin_display_prefs = function_exists('soo_plugin_pref_vals') ? 
+	soo_plugin_pref_vals('soo_plugin_display') : array();
+foreach ( soo_plugin_display_defaults() as $name => $arr )
+		// just in case prefs/defaults do not match;
+		// might happen if plugin upgraded while soo_plugin_pref disabled
+	if ( ! array_key_exists($name, $soo_plugin_display_prefs) )
+		$soo_plugin_display_prefs[$name] = $arr['val'];
 
+	// callback for plugin_prefs/plugin_lifecycle events
 function soo_plugin_display_prefs( $event, $step ) {
 	if ( function_exists('soo_plugin_pref') )
 		return soo_plugin_pref($event, $step, soo_plugin_display_defaults());
+	
+		// message to install soo_plugin_pref
 	if ( substr($event, 0, 12) == 'plugin_prefs' ) {
-		$plugin = substr($event, 12);
+		$plugin = substr($event, 13);
 		$message = '<p><br /><strong>' . gTxt('edit') . " $plugin " . 
 			gTxt('edit_preferences') . ':</strong><br />' . gTxt('install_plugin') . 
 			' <a href="http://ipsedixit.net/txp/92/soo_plugin_pref">soo_plugin_pref</a></p>';
@@ -74,6 +80,11 @@ function soo_plugin_display_defaults( ) {
 			'html'	=>	'text_input',
 			'text'	=>	'Text to append to line numbers (leave blank to hide numbers)',
 		),
+		'tab_stop'			=>	array(
+			'val'	=>	4,
+			'html'	=>	'text_input',
+			'text'	=>	'Spaces per tab in plugin code',
+		),
 	);
 }
 
@@ -81,9 +92,9 @@ function soo_plugin_display_defaults( ) {
  //									Tags								//
 //---------------------------------------------------------------------//
 
-// tag to select plugin(s) for display
-// can be used as single w/form or container
-// when getting multiple plugins code and help are not retrieved
+	// tag to select plugin(s) for display
+	// can be used as single w/form or container
+	// for plugin lists code and help are not retrieved
 function soo_plugin_display( $atts, $thing = '' ) {
 	global $soo_plugin_display, $soo_plugin_display_prefs;
 	extract(lAtts(array(
@@ -120,45 +131,48 @@ function soo_plugin_display( $atts, $thing = '' ) {
 		return doWrap($out, $wraptag, $break, $class, '', '', '', $html_id);
 }
 
-// basic output tags: display field direct from database
+	// basic output tags: display field direct from database
 function soo_plugin_author_uri( ) { return _soo_plugin_field('author_uri'); }
 function soo_plugin_version( ) { return _soo_plugin_field('version'); }
 function soo_plugin_description( ) { return _soo_plugin_field('description'); }
 
-// as above, but with option to make output a link to author's website
+	// as above, but with option to make output a link to author's website
 function soo_plugin_author( $atts ) { return _soo_plugin_field('author_uri', $atts); }
 function soo_plugin_name( $atts ) { return _soo_plugin_field('name', $atts); }
 
-// display plugin help, with options to remove leading style and/or h1 elements,
-// and option to restrict display to named section
+	// display plugin help, with options to remove leading style and/or h1 elements,
+	// and option to restrict display to named section
 function soo_plugin_help( $atts ) { 
 	global $soo_plugin_display_prefs;
 	extract(lAtts(array(
 		'strip_style'	=>	$soo_plugin_display_prefs['strip_style'],
 		'strip_title'	=>	$soo_plugin_display_prefs['strip_title'],
 		'section_id'	=>	'',		// HTML id for header element to display
+		'h_level'		=>	0,		// if section_id, transpose header levels starting here
 	), $atts));
 	$help = _soo_plugin_field('help');
 	
-	// remove leading <style> element
+		// remove leading <style> element
 	if ( $strip_style and preg_match('/^<style/', $help) )
 		$help = preg_replace('/^[\s\S]+?<\/style>([\s\S]+)$/', '$1', $help);
 		
-	// remove leading <h1> element
+		// remove leading <h1> element
 	if ( $strip_title )
 		$help = preg_replace('/^([\s\S]+?)<h1[\s\S]+?<\/h1>([\s\S]+)$/', '$1$2', $help);
 	
-	// display from named header element to next header with same or lower h-level
+		// display from named header element to next header with same or lower h-level
 	if ( $section_id ) {
-		$pattern = "/^([\s\S]+)(<h(\d)[^>]+id=['\"]$section_id('|\").+?>[\s\S]+)$/";
+		$pattern = "/^([\s\S]+)<h(\d)([^>]+id=['\"]$section_id('|\").+?>[\s\S]+)$/";
 		if ( preg_match($pattern, $help, $match) ) {
-			list( , , $remainder, $h_level) = $match;
+			list( , , $h_num, $remainder) = $match;
 			$help = '';
-			$pattern = '/^([\s\S]+?)(<h(\d)[\s\S]+)$/';
+			$pattern = '/^([\s\S]+?)<h(\d)([\s\S]+)$/';
+			$remainder = "<h$h_num$remainder";
 			while ( preg_match($pattern, $remainder, $match) ) {
-				list( , $keep, $remainder, $next_level) = $match;
+				list( , $keep, $next_h, $remainder) = $match;
 				$help .= $keep;
-				if ( $h_level >= $next_level )
+				$remainder = "<h$next_h$remainder";
+				if ( $h_num >= $next_h )
 					$remainder = '';
 			}
 			if ( ! $help )
@@ -168,7 +182,7 @@ function soo_plugin_help( $atts ) {
 	return $help;
 }
 
-// display installed code size in KB
+	// display installed code size in KB
 function soo_plugin_size( $atts ) {
 	global $soo_plugin_display_prefs;
 	extract(lAtts(array(
@@ -178,23 +192,24 @@ function soo_plugin_size( $atts ) {
 	return str_replace('{size}', $size, $format);
 }
 
-// display plugin source code
-// code highlighting with named classes for styling
-// various options for line numbering
-// display full source or named function, class, or method
+	// display plugin source code
+	// code highlighting with named classes for styling
+	// various options for line numbering
+	// display full source or named function, class, or method
 function soo_plugin_code( $atts ) {
 	global $soo_plugin_display_prefs;
 	extract(lAtts(array(
 		'show_line_numbers'		=>	$soo_plugin_display_prefs['show_line_numbers'],
-		'reindex_lines'	=>	0,
+		'reindex_lines'			=>	0,
+		'tab_stop'				=>	$soo_plugin_display_prefs['tab_stop'],
 		'function'				=>	'',
 		'php_class'				=>	'',
 		'class'					=>	'soo_plugin_code',
 		'html_id'				=>	'',
 	), $atts));
 	
-	// names of code highlight types from ini_get()
-	// highlight_string() output style attributes will be replaced by named classes
+		// names of code highlight types from ini_get()
+		// highlight_string() output style attributes will be replaced by named classes
 	$keys = array('bg', 'comment', 'default', 'html', 'keyword', 'string');
 	foreach ( $keys as $k ) {
 		$find[] = 'style="color: ' . ini_get("highlight.$k") . '"';
@@ -208,7 +223,7 @@ function soo_plugin_code( $atts ) {
 	$match_index = 2;
 	$safety_check = true;
 	
-	// find named method: assumes end brace one tab in from start of line
+		// find named method: assumes end brace one tab in from start of line
 	if ( $php_class and $function ) {
 		$pattern = '/([\s\S]*(abstract|)\s*class\s+' 
 			. $php_class . '\s+.*?\{[\s\S]+?)(\t(public|private|protected)?\s*function\s+' 
@@ -216,12 +231,12 @@ function soo_plugin_code( $atts ) {
 		$match_index ++;
 	}
 	
-	// find named function: assumes end brace at start of line
+		// find named function: assumes end brace at start of line
 	elseif ( $function )
 		$pattern = '/([\s\S]*)((public|private|protected)?\s*function\s+' 
 			. $function . '\s*\([\s\S]+?\n}.*)/';
 	
-	// find named class: assumes end brace at start of line
+		// find named class: assumes end brace at start of line
 	elseif ( $php_class )
 		$pattern = '/([\s\S]*)((abstract|)\s*class\s+' 
 			. $php_class . '\s+.*?\{[\s\S]+?\n}.*)[\s\S]*/';
@@ -229,7 +244,7 @@ function soo_plugin_code( $atts ) {
 	else
 		$safety_check = false;
 	
-	// safety check is because the preg_match() can take a long time if there is no match
+		// safety check is because the preg_match() can take a long time if there is no match
 	if ( $safety_check ) {
 		if ( $function and ! preg_match("/$function\s*\(/", $raw_code) )
 			return;
@@ -241,7 +256,7 @@ function soo_plugin_code( $atts ) {
 		if ( preg_match($pattern, $raw_code, $match) ) {
 			$raw_code = $match[$match_index];
 			
-			// if not reindexing, find starting line number in full source code
+				// if not reindexing, find starting line number in full source code
 			$start_line = $reindex_lines ? 
 				$reindex_lines : count(explode("\n", $match[1]));
 		}
@@ -249,7 +264,9 @@ function soo_plugin_code( $atts ) {
 			return;
 	}
 	
-	$lines = explode("\n", $raw_code);
+		// convert tabs to spaces
+	$lines = _soo_detab(explode("\n", $raw_code), $tab_stop);
+	
 	$total_lines = $start_line + count($lines) - 1;
 	foreach ( $lines as $i => $line ) {
 		if ( preg_match('/\S/', $line) )
@@ -287,6 +304,26 @@ function _soo_plugin_field( $field, $atts = null ) {
 		$out = $soo_plugin_display[$field];
 		return ( ! empty($link) and $author_uri ) ? href($out, $author_uri) : $out;
 	}
+}
+
+	// convert tabs to spaces, aligned to tab stops
+function _soo_detab( $lines, $tab_stop ) {
+	$out = array();
+	foreach ( $lines as $line ) {
+		$bucket = array();
+		foreach ( str_split($line) as $char )
+			if ( $char == t ) {
+				$add = $tab_stop - fmod(count($bucket), $tab_stop);
+				while ( $add ) {
+					$bucket[] = ' ';
+					$add --;
+				}
+			}
+			else
+				$bucket[] = $char;
+		$out[] = implode('', $bucket);
+	}
+	return $out;
 }
 
 # --- END PLUGIN CODE ---
@@ -425,13 +462,13 @@ pre. <txp:soo_plugin_help />
 
 h4. Attributes
 
-* @strip_style@ _(boolean)_ whether or not to remove any leading &lt;style> element (%(default)default% "1", can be changed in prefs)
-* @strip_title@ _(boolean)_ whether or not to remove any leading &lt;h1> element (%(default)default% "1", can be changed in prefs)
+* @strip_style@ _(boolean)_ whether or not to remove any leading @<style>@ element (%(default)default% "1", can be changed in prefs)
+* @strip_title@ _(boolean)_ whether or not to remove any leading @<h1>@ element (%(default)default% "1", can be changed in prefs)
 * @section_id@ _(HTML id value)_ start display from identified header element, continue till next header of same or lower level
 
 @strip_style@ looks for an opening @<style>@ tag at the very start of the Help section. @strip_title@ looks for the first occurence of @<h1>@. Both do a non-greedy match looking for the closing tag.
 
-With @section_id@, display begins from the HTML header element with the specified id. Display continues till the next HTML header with the same or lower level (<h2> considered lower than <h3>) or until the end if no such header is found.
+With @section_id@, display begins from the HTML header element with the specified id. Display continues till the next HTML header with the same or lower level (@<h2>@ considered lower than @<h3>@, for example) or until the end if no such header is found.
 
 h3(#soo_plugin_code). soo_plugin_code
 
@@ -445,6 +482,7 @@ h4. Attributes
 
 * @show_line_numbers@ _(text)_ If set, text to append to each line number. If blank, do not show line numbers. %(default)default% ":" (can be changed in prefs).
 * @reindex_lines@ _(integer)_ With @function@ and/or @php_class@, renumber lines starting from the value given. %(default)Default% "0", do not reindex.
+* @tab_stop@ _(integer)_ length of tab stop
 * @function@ _(text)_ Show only this function. Use in combination with @php_class@ to show only this method.
 * @php_class@ _(text)_ Show only this PHP class
 * @class@ _(html class name)_ for &lt;code&gt; element
@@ -460,6 +498,8 @@ The code highlighting is based on the PHP @highlight_string()@ function. The @st
 * @php_string@ strings
 
 Everything but the line numbers will be in a @span@ with one of those class names. The whole thing is wrapped in a @code@ element.
+
+Tabs are converted to spaces, to stay aligned with tab stops as set in plugin preferences or the @tag_stop@ attribute.
 
 h2(#examples). Examples
 
@@ -504,6 +544,10 @@ If you have the "soo_plugin_pref":http://ipsedixit.net/txp/92/soo_plugin_pref pr
 * Default value for @soo_plugin_code@'s @show_line_numbers@ attribute
 
 h2(#history). Version History
+
+h3. 0.1.3 (9/27/2009)
+
+For @soo_plugin_code@, tab to space conversion now maintains tab-stop alignment
 
 h3. 0.1.2 (9/26/2009)
 
